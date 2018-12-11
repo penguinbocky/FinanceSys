e1c281b6b4c96f38860c7ef1aa8997d950b00401
penguinbocky <1553308584@qq.com>
2018-12-11 22:56:11 +0800

    Delete ok button and use real time calculation, display result in label rather than a pop up which blocks action.

--- src/pers/bocky/finance/view/impl/ReportPanel.java
+++ src/pers/bocky/finance/view/impl/ReportPanel.java
@@ -4,8 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.ArrayList;
@@ -15,9 +13,8 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.swing.ButtonGroup;
-import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
@@ -48,7 +45,7 @@ public class ReportPanel extends JPanel implements WillBeInMainTabbed {
 	private Optional<List<TypeBean>> selectedTypes;
 
 	private JPanel panelForCheckbox;
-	private JButton okBtn = new JButton("计算");;
+	private JLabel resultText;
 	
 	public ReportPanel() {
 		super();
@@ -65,7 +62,7 @@ public class ReportPanel extends JPanel implements WillBeInMainTabbed {
 	private void createUIAndLoadOnce() {
 		loadDatagrid();// Init static data first
 		add(createTopPanel(), BorderLayout.NORTH);
-		add(createMiddlePanel(), BorderLayout.CENTER);
+		add(createMiddlePanel(false), BorderLayout.CENTER);
 		add(createBottomPanel(), BorderLayout.SOUTH);
 	}
 
@@ -89,12 +86,13 @@ public class ReportPanel extends JPanel implements WillBeInMainTabbed {
 				public void itemStateChanged(ItemEvent e) {
 					if (e.getStateChange() == ItemEvent.SELECTED) {
 						selectedCategory = categoryBean;
-						System.out.println(selectedCategory + "" + selectedCategory.getCategoryId());
+						System.out.println(selectedCategory + " > " + selectedCategory.getCategoryId());
 						panelForCheckbox.removeAll();
 //						panelForCheckbox.invalidate();
-						add(createMiddlePanel(), BorderLayout.CENTER);
+						add(createMiddlePanel(false), BorderLayout.CENTER);
 						panelForCheckbox.updateUI();
 //						panelForCheckbox.repaint();
+						tryCalculate();
 					}
 
 				}
@@ -103,14 +101,109 @@ public class ReportPanel extends JPanel implements WillBeInMainTabbed {
 		return panelForRadios;
 	}
 
-	private JPanel createMiddlePanel() {
+	private void tryCalculate() {
+		System.out.println("tryCalculate > " + selectedTypes.isPresent() + ", " + (selectedTypes.isPresent() ? selectedTypes.get() : "not present"));
+		if (selectedTypes.isPresent() && selectedTypes.get().size() > 0) {
+			System.out.println("none null");
+			System.out.println("selected values:");
+			System.out.println(selectedTimeOption);
+			System.out.println(selectedCategory + "" + selectedCategory.getCategoryId());
+			System.out.println(selectedTypes);
+
+			Integer[] selectedTypeIds = selectedTypes.get().stream().map(bean -> bean.getTypeId()).toArray(Integer[]::new);
+			double result = 0;
+			switch (selectedCategory.getCategoryId()) {
+			case 1://deposit
+				switch (selectedTimeOption) {
+				case LATEST_30:
+					result = DepositDao.calculateAmountOfLatestMonthOfType(selectedTypeIds);
+					break;
+				case LAST_MONTH:
+					result = DepositDao.calculateAmountOfLastMonthhOfType(selectedTypeIds);
+					break;
+				case AVG_MONTH:
+					result = DepositDao.calculateAvgMonthAmountOfType(selectedTypeIds);
+					break;
+				case CURRENT_MONTH:
+					result = DepositDao.calculateAmountFromThisMonthOfType(selectedTypeIds);
+					break;
+				default:
+					break;
+				}
+				break;
+			case 2://consume
+				switch (selectedTimeOption) {
+				case LATEST_30:
+					result = ConsumeDao.calculateAmountOfLatestMonthOfType(selectedTypeIds);
+					break;
+				case LAST_MONTH:
+					result = ConsumeDao.calculateAmountOfLastMonthhOfType(selectedTypeIds);
+					break;
+				case AVG_MONTH:
+					result = ConsumeDao.calculateAvgMonthAmountOfType(selectedTypeIds);
+					break;
+				case CURRENT_MONTH:
+					result = ConsumeDao.calculateAmountFromThisMonthOfType(selectedTypeIds);
+					break;
+				default:
+					break;
+				}
+				break;
+			case 3://borrow
+				switch (selectedTimeOption) {
+				case LATEST_30:
+					result = BorrowDao.calculateAmountOfLatestMonthOfType(selectedTypeIds);
+					break;
+				case LAST_MONTH:
+					result = BorrowDao.calculateAmountOfLastMonthhOfType(selectedTypeIds);
+					break;
+				case AVG_MONTH:
+					result = BorrowDao.calculateAvgMonthAmountOfType(selectedTypeIds);
+					break;
+				case CURRENT_MONTH:
+					result = BorrowDao.calculateAmountFromThisMonthOfType(selectedTypeIds);
+					break;
+				default:
+					break;
+				}
+				break;
+			case 4://lend
+				switch (selectedTimeOption) {
+				case LATEST_30:
+					result = LendDao.calculateAmountOfLatestMonthOfType(selectedTypeIds);
+					break;
+				case LAST_MONTH:
+					result = LendDao.calculateAmountOfLastMonthhOfType(selectedTypeIds);
+					break;
+				case AVG_MONTH:
+					result = LendDao.calculateAvgMonthAmountOfType(selectedTypeIds);
+					break;
+				case CURRENT_MONTH:
+					result = LendDao.calculateAmountFromThisMonthOfType(selectedTypeIds);
+					break;
+				default:
+					break;
+				}
+				break;
+
+			default:
+				break;
+			}//end switch
+			
+			//JOptionPane.showMessageDialog(null, "Result: " + result + (selectedCategory.getCategoryId() == 2 ? "\nNote: 2018.5 and 6 and current month are not taken into account." : ""), selectedTimeOption.getTimeOptionName() + " For " + selectedCategory, JOptionPane.INFORMATION_MESSAGE);
+//		}
+			
+			resultText.setText(selectedTimeOption.getTimeOptionName() + " for " + selectedCategory.getCategoryName() + ": " + result);
+		}//end if
+	}
+
+	private JPanel createMiddlePanel(boolean allChecked) {
 		panelForCheckbox = new JPanel(new GridLayout(5, 0));
 		panelForCheckbox.setBackground(new Color(208, 223, 239));
 		selectedTypes = Optional.ofNullable(typeDefsMap.get(selectedCategory.getCategoryId()));
 		if (selectedTypes.isPresent()) {
-			okBtn.setEnabled(true);
 			selectedTypes.get().stream().forEach(typeBean -> {
-				JCheckBox checkBox = new JCheckBox(typeBean.getTypeName(), false);
+				JCheckBox checkBox = new JCheckBox(typeBean.getTypeName(), allChecked);
 				checkBox.setOpaque(false);
 				panelForCheckbox.add(checkBox);
 				checkBox.addItemListener(new ItemListener() {
@@ -124,15 +217,15 @@ public class ReportPanel extends JPanel implements WillBeInMainTabbed {
 							System.out.println(typeBean.getTypeName() + " unchecked");
 							selectedTypes.get().remove(typeBean);
 						}
+						tryCalculate();
 					}
 				});
 			});
-			
-		} else {
-			okBtn.setEnabled(false);
+			if (!allChecked) {
+				selectedTypes.get().clear();
+			}
 		}
 		
-		selectedTypes.get().clear();
 		return panelForCheckbox;
 	}
 
@@ -160,110 +253,15 @@ public class ReportPanel extends JPanel implements WillBeInMainTabbed {
 						System.out.println(timeOption.toString());
 						selectedTimeOption = timeOption;
 					}
-
+					tryCalculate();
 				}
 			});
 		});
 
 		JPanel panelForBtn = new JPanel();
-		okBtn.setBackground(new Color(49, 126, 243));
-		okBtn.setForeground(new Color(255, 255, 255));
-		okBtn.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("selected values:");
-				System.out.println(selectedTimeOption);
-				System.out.println(selectedCategory + "" + selectedCategory.getCategoryId());
-				System.out.println(selectedTypes);
-
-				Integer[] selectedTypeIds = selectedTypes.get().stream().map(bean -> bean.getTypeId()).toArray(Integer[]::new);
-				double result = 0;
-				switch (selectedCategory.getCategoryId()) {
-				case 1://deposit
-					switch (selectedTimeOption) {
-					case LATEST_30:
-						result = DepositDao.calculateAmountOfLatestMonthOfType(selectedTypeIds);
-						break;
-					case LAST_MONTH:
-						result = DepositDao.calculateAmountOfLastMonthhOfType(selectedTypeIds);
-						break;
-					case AVG_MONTH:
-						result = DepositDao.calculateAvgMonthAmountOfType(selectedTypeIds);
-						break;
-					case CURRENT_MONTH:
-						result = DepositDao.calculateAmountFromThisMonthOfType(selectedTypeIds);
-						break;
-					default:
-						break;
-					}
-					break;
-				case 2://consume
-					switch (selectedTimeOption) {
-					case LATEST_30:
-						result = ConsumeDao.calculateAmountOfLatestMonthOfType(selectedTypeIds);
-						break;
-					case LAST_MONTH:
-						result = ConsumeDao.calculateAmountOfLastMonthhOfType(selectedTypeIds);
-						break;
-					case AVG_MONTH:
-						result = ConsumeDao.calculateAvgMonthAmountOfType(selectedTypeIds);
-						break;
-					case CURRENT_MONTH:
-						result = ConsumeDao.calculateAmountFromThisMonthOfType(selectedTypeIds);
-						break;
-					default:
-						break;
-					}
-					break;
-				case 3://borrow
-					switch (selectedTimeOption) {
-					case LATEST_30:
-						result = BorrowDao.calculateAmountOfLatestMonthOfType(selectedTypeIds);
-						break;
-					case LAST_MONTH:
-						result = BorrowDao.calculateAmountOfLastMonthhOfType(selectedTypeIds);
-						break;
-					case AVG_MONTH:
-						result = BorrowDao.calculateAvgMonthAmountOfType(selectedTypeIds);
-						break;
-					case CURRENT_MONTH:
-						result = BorrowDao.calculateAmountFromThisMonthOfType(selectedTypeIds);
-						break;
-					default:
-						break;
-					}
-					break;
-				case 4://lend
-					switch (selectedTimeOption) {
-					case LATEST_30:
-						result = LendDao.calculateAmountOfLatestMonthOfType(selectedTypeIds);
-						break;
-					case LAST_MONTH:
-						result = LendDao.calculateAmountOfLastMonthhOfType(selectedTypeIds);
-						break;
-					case AVG_MONTH:
-						result = LendDao.calculateAvgMonthAmountOfType(selectedTypeIds);
-						break;
-					case CURRENT_MONTH:
-						result = LendDao.calculateAmountFromThisMonthOfType(selectedTypeIds);
-						break;
-					default:
-						break;
-					}
-					break;
-
-				default:
-					break;
-				}
-				
-				JOptionPane.showMessageDialog(null, "Result: " + result + (selectedCategory.getCategoryId() == 2 ? "\nNote: 2018.5 and 6 and current month are not taken into account." : ""), selectedTimeOption.getTimeOptionName() + " For " + selectedCategory, JOptionPane.INFORMATION_MESSAGE);
-			}
-		});
-		
-		JButton resetBtn = new JButton("重置");
-		panelForBtn.add(resetBtn);
-		panelForBtn.add(okBtn);
+		resultText = new JLabel();
+		resultText.setForeground(Color.RED);
+		panelForBtn.add(resultText);
 
 		layoutPanel.add(panelForRadios2);
 		layoutPanel.add(panelForBtn);
